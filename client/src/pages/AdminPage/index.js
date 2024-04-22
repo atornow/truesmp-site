99b2d2f35dae06fafd836c3bf09d0532df5105e5
Armaan <atornow22@gmail.com>
2024-04-22 11:22:29 -0700

    Improve adminpage ui

--- client/src/pages/AdminPage/index.js
+++ client/src/pages/AdminPage/index.js
@@ -1,17 +1,40 @@
 import React from 'react';
+import styled from 'styled-components';
 import ChallengePanel from './ChallengePanel';
 import DeletePanel from './DeletePanel';
 import ChallengeRoadPanel from './ChallengeRoadPanel';
 import NewsPanel from './NewsPanel';
 
+const AdminPageContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 1rem;
+`;
+
+const PanelContainer = styled.div`
+  flex: 1 1 400px;
+  padding: 20px;
+  background-color: #fff;
+`;
+
 function AdminPage() {
   return (
-    <div>
+    <div style={{padding: '2rem'}}>
       <h2>Admin Page</h2>
-      <ChallengePanel />
-      <DeletePanel />
-      <ChallengeRoadPanel />
-      <NewsPanel />
+      <AdminPageContainer>
+        <PanelContainer className="border doodle-border-2">
+          <ChallengePanel />
+        </PanelContainer>
+        <PanelContainer className="border doodle-border-2">
+          <DeletePanel />
+        </PanelContainer>
+        <PanelContainer className="border doodle-border-2">
+          <ChallengeRoadPanel />
+        </PanelContainer>
+        <PanelContainer className="border doodle-border-2">
+          <NewsPanel />
+        </PanelContainer>
+      </AdminPageContainer>
     </div>
   );
 }
