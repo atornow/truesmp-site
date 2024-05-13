e476de7004d7a4070941a26b0165373de3814f7f
Armaan <atornow22@gmail.com>
2024-05-13 08:10:50 -0700

    Update banner with interior color box

--- client/src/elements/ProfileBanner.js
+++ client/src/elements/ProfileBanner.js
@@ -2,16 +2,15 @@ import React from 'react';
 import styled from 'styled-components';
 
 const BannerContainer = styled.div`
+  position: relative;
   display: flex;
   justify-content: center;
   align-items: center;
   width: 100%;
   height: 100px;
-  background-color: #f0f0f0;
   margin-bottom: 1rem;
   box-sizing: border-box;
-  border-radius: 10px;
-  overflow: hidden;
+  border-radius: 12px;
 
   @media (max-width: 768px) {
     height: 80px;
@@ -22,14 +21,32 @@ const BannerContent = styled.div`
   text-align: center;
 `;
 
+const BannerColorBox = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+  top: -8px;
+  left: -8px;
+  right: -8px;
+  bottom: -8px;
+  background-color: #FFAA00;
+  box-sizing: border-box;
+  border-radius: 7px;
+
+
+`;
+
 function ProfileBanner() {
     return (
         <BannerContainer className="border doodle-border">
+            <BannerColorBox>
             <BannerContent>
                 {/* Add your banner content here */}
                 <h3>News Banner</h3>
                 {/* You can add more elements or components as needed */}
             </BannerContent>
+            </BannerColorBox>
         </BannerContainer>
     );
 }
